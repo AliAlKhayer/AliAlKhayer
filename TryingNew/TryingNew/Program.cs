40c69f709ffd4c87b1dfff4b9a35fd2e0c1bcb95
Ali Al Khayer <Ali.Al.Khayer-1@ou.edu>
2020-01-22 11:09:39 -0600

    Started a new repo and name

---
+++ TryingNew/TryingNew/Program.cs
@@ -0,0 +1,16 @@
+﻿//Ali Al Khayer
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryingNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
